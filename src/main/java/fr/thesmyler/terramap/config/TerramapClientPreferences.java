fb40e600969b7c472f48f75bb4f05b2079b53717
Smyler <smyler@mail.com>
2020-11-11 19:06:34 +0100

    Better error handling in client preferences

--- src/main/java/fr/thesmyler/terramap/config/TerramapClientPreferences.java
+++ src/main/java/fr/thesmyler/terramap/config/TerramapClientPreferences.java
@@ -26,56 +26,94 @@ public class TerramapClientPreferences {
 
 	public static final String FILENAME = "terramap_client_preferences.json";
 	private static File file = null;
-	private static Preferences preferences = new Preferences();
+	private static ClientPreferences preferences = new ClientPreferences();
+	
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
 
 	public static String getServerGenSettings(String serverId) {
-		return preferences.servers.containsKey(serverId) ? preferences.servers.get(serverId).genSettings: "";
+		try {
+			if(preferences == null || preferences.servers == null) {
+				preferences = new ClientPreferences();
+			}
+			return preferences.servers.containsKey(serverId) ? preferences.servers.get(serverId).genSettings: "";
+		} catch(Exception e) {
+			TerramapMod.logger.warn("Failed to get server gen settings");
+			TerramapMod.logger.catching(e);
+			return "";
+		}
 	}
-	
+
 	public static TerramapScreenSavedState getServerSavedScreen(String serverId) {
-		return preferences.servers.containsKey(serverId) ? preferences.servers.get(serverId).mapState: null;
+		try {
+			if(preferences == null || preferences.servers == null) {
+				preferences = new ClientPreferences();
+			}
+			return preferences.servers.containsKey(serverId) ? preferences.servers.get(serverId).mapState: null;
+		} catch(Exception e) {
+			TerramapMod.logger.warn("Failed to get saved screen state");
+			TerramapMod.logger.catching(e);
+			return null;
+		}
 	}
 
 	public static void setServerSavedScreen(String serverId, TerramapScreenSavedState mapState) {
-		ServerPreferences serv = preferences.servers.getOrDefault(serverId, new ServerPreferences());
-		serv.mapState = mapState;
-		if(!preferences.servers.containsKey(serverId)) preferences.servers.put(serverId, serv);
+		try {
+			if(preferences == null || preferences.servers == null) {
+				preferences = new ClientPreferences();
+			}
+			ServerPreferences serv = preferences.servers.getOrDefault(serverId, new ServerPreferences());
+			serv.mapState = mapState;
+			preferences.servers.put(serverId, serv);
+		} catch(Exception e) {
+			TerramapMod.logger.warn("Failed to set saved screen state");
+			TerramapMod.logger.catching(e);
+		}
 	}
-	
+
 	public static void setServerGenSettings(String serverId, String genSettings) {
-		ServerPreferences serv = preferences.servers.getOrDefault(serverId, new ServerPreferences());
-		serv.genSettings = genSettings;
-		if(!preferences.servers.containsKey(serverId)) preferences.servers.put(serverId, serv);
+		try {
+			if(preferences == null || preferences.servers == null) {
+				preferences = new ClientPreferences();
+			}
+			ServerPreferences serv = preferences.servers.getOrDefault(serverId, new ServerPreferences());
+			serv.genSettings = genSettings;
+			if(!preferences.servers.containsKey(serverId)) preferences.servers.put(serverId, serv);
+		} catch(Exception e) {
+			TerramapMod.logger.warn("Failed to set gen settings");
+			TerramapMod.logger.catching(e);
+		}
 	}
 
 	public static void save() {
-		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String str = gson.toJson(preferences);
 		try {
+			if(preferences == null || preferences.servers == null) {
+				preferences = new ClientPreferences();
+			}
+			String str = GSON.toJson(preferences);
 			Files.write(str, file, Charset.defaultCharset());
 		} catch (IOException e) {
 			TerramapMod.logger.error("Failed to write client preferences to " + file.getAbsolutePath());
-			e.printStackTrace();
+			TerramapMod.logger.catching(e);
 		}
 	}
 
 	public static void load() {
 		if(!file.exists()) {
-			preferences = new Preferences();
+			preferences = new ClientPreferences();
 			TerramapMod.logger.info("Client preference file did not exist, used default");
 		} else {
 			try {
 				String text = String.join("\n", Files.readLines(file, Charset.defaultCharset()));
 				Gson gson = new Gson();
-				preferences = gson.fromJson(text, Preferences.class);
+				preferences = gson.fromJson(text, ClientPreferences.class);
 			} catch (IOException | JsonSyntaxException e) {
 				TerramapMod.logger.error("Failed to load client preference file, setting to default");
 				TerramapMod.logger.catching(e);
-				preferences = new Preferences();
+				preferences = new ClientPreferences();
 			}
 		}
 	}
-	
+
 	public static void setFile(File file) {
 		if(TerramapClientPreferences.file == null) {
 			TerramapClientPreferences.file = file;
@@ -83,14 +121,14 @@ public class TerramapClientPreferences {
 			TerramapMod.logger.error("Tried to set client preference file but it was already");
 		}
 	}
-	
-	private static class Preferences {
+
+	private static class ClientPreferences {
 		Map<String, ServerPreferences> servers = new HashMap<String, ServerPreferences>();
 	}
-	
+
 	private static class ServerPreferences {
 		public String genSettings = "";
 		public TerramapScreenSavedState mapState = new TerramapScreenSavedState();
 	}
-	
+
 }
