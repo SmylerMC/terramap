5b6b04c2256c113170b7dd63e69011991dd4c9de
Smyler <smyler@mail.com>
2021-05-28 21:46:59 +0200

    Cache rendered regions for better performance

--- src/main/java/fr/thesmyler/terramap/util/Vec2d.java
+++ src/main/java/fr/thesmyler/terramap/util/Vec2d.java
@@ -80,4 +80,27 @@ public final class Vec2d {
         return new double[] {this.x, this.y};
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        long temp;
+        temp = Double.doubleToLongBits(x);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(y);
+        result = prime * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true;
+        if(obj == null) return false;
+        if(getClass() != obj.getClass()) return false;
+        Vec2d other = (Vec2d) obj;
+        if(Double.doubleToLongBits(x) != Double.doubleToLongBits(other.x)) return false;
+        if(Double.doubleToLongBits(y) != Double.doubleToLongBits(other.y)) return false;
+        return true;
+    }
+
 }
