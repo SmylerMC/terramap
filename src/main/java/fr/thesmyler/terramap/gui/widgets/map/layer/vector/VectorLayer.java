4693495da5ccf2652e2ec46b56dc7e9f808f6e8a
Smyler <smyler@mail.com>
2021-07-19 15:17:24 +0200

    Vector layer: only render visible features and debug, fix render bounds

--- src/main/java/fr/thesmyler/terramap/gui/widgets/map/layer/vector/VectorLayer.java
+++ src/main/java/fr/thesmyler/terramap/gui/widgets/map/layer/vector/VectorLayer.java
@@ -1,19 +1,23 @@
 package fr.thesmyler.terramap.gui.widgets.map.layer.vector;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.UUID;
 
 import fr.thesmyler.smylibgui.container.WidgetContainer;
+import fr.thesmyler.smylibgui.util.Color;
+import fr.thesmyler.smylibgui.util.RenderUtil;
 import fr.thesmyler.terramap.gui.widgets.map.MapLayer;
 import fr.thesmyler.terramap.gui.widgets.map.MapWidget;
-import fr.thesmyler.terramap.maps.vector.Geometry;
-import fr.thesmyler.terramap.maps.vector.LineString;
-import fr.thesmyler.terramap.maps.vector.MultiGeometry;
-import fr.thesmyler.terramap.maps.vector.MultiLineString;
-import fr.thesmyler.terramap.maps.vector.MultiPoint;
-import fr.thesmyler.terramap.maps.vector.MultiPolygon;
-import fr.thesmyler.terramap.maps.vector.Point;
-import fr.thesmyler.terramap.maps.vector.Polygon;
-import fr.thesmyler.terramap.maps.vector.VectorFeature;
+import fr.thesmyler.terramap.maps.vector.features.LineString;
+import fr.thesmyler.terramap.maps.vector.features.MultiGeometry;
+import fr.thesmyler.terramap.maps.vector.features.MultiLineString;
+import fr.thesmyler.terramap.maps.vector.features.MultiPoint;
+import fr.thesmyler.terramap.maps.vector.features.MultiPolygon;
+import fr.thesmyler.terramap.maps.vector.features.Point;
+import fr.thesmyler.terramap.maps.vector.features.Polygon;
+import fr.thesmyler.terramap.maps.vector.features.VectorFeature;
 import fr.thesmyler.terramap.util.geo.GeoBounds;
 import fr.thesmyler.terramap.util.geo.GeoPoint;
 import fr.thesmyler.terramap.util.math.Vec2d;
@@ -24,6 +28,10 @@ import net.minecraft.profiler.Profiler;
 public abstract class VectorLayer extends MapLayer {
     
     private static final int MAX_FEATURE_DEPTH = 50;
+    
+    private int pointsRendered = 0;
+    private int linesRendered = 0;
+    private int polygonsRendered = 0;
 
     public VectorLayer(double tileScaling) {
         super(tileScaling);
@@ -34,6 +42,7 @@ public abstract class VectorLayer extends MapLayer {
 
         MapWidget parentMap = (MapWidget) parent;
         Profiler profiler = parentMap.getProfiler();
+        boolean debug = parentMap.isDebugMode();
 
         profiler.startSection("render-vector-layer_" + this.getId());
 
@@ -41,15 +50,36 @@ public abstract class VectorLayer extends MapLayer {
         double extendedWidth = this.getExtendedWidth();
         double extendedHeight = this.getExtendedHeight();
 
-        Vec2d upperLeft = this.getUpperLeftRenderCorner();
-        GeoPoint lowerLocation = this.getRenderLocation(upperLeft.add(0, extendedHeight));
-        GeoPoint upperLocation = this.getRenderLocation(upperLeft.add(extendedWidth, 0));
+        GeoPoint lowerLocation = this.getRenderLocation(new Vec2d(0, extendedHeight));
+        GeoPoint upperLocation = this.getRenderLocation(new Vec2d(extendedWidth, 0));
         GeoBounds renderBounds = new GeoBounds(lowerLocation, upperLocation);
         
-        Iterator<VectorFeature> features = this.getVisibleFeatures(renderBounds);
+        this.pointsRendered = 0;
+        this.linesRendered = 0;
+        this.polygonsRendered = 0;
+        
+        Iterator<VectorFeature> featureIterator = this.getVisibleFeatures(renderBounds);
         GlStateManager.pushMatrix();
         this.applyRotationGl(x, y);
-        while(features.hasNext()) this.drawFeature(features.next(), 0);
+        Map<UUID, VectorFeature> features = new HashMap<>();
+        while(featureIterator.hasNext()) {
+            VectorFeature feature = featureIterator.next();
+            if(renderBounds.intersects(feature.bounds()))
+                features.put(feature.uid(), feature);
+        }
+        
+        for(VectorFeature feature: features.values()) this.drawFeature(feature, 0);
+        
+        if(debug) {
+            Vec2d lowerCorner = this.getRenderPos(lowerLocation);
+            Vec2d upperCorner = this.getRenderPos(upperLocation);
+            RenderUtil.drawClosedStrokeLine(Color.YELLOW, 5f,
+                    lowerCorner.x, lowerCorner.y,
+                    upperCorner.x, lowerCorner.y,
+                    upperCorner.x, upperCorner.y,
+                    lowerCorner.x, upperCorner.y
+                    );
+        }
         GlStateManager.popMatrix();
         profiler.endSection();
     }
@@ -65,8 +95,6 @@ public abstract class VectorLayer extends MapLayer {
             this.drawMultiLineString((MultiLineString) feature);
         } else if(feature instanceof MultiPoint) {
             this.drawMultiPoint((MultiPoint) feature);
-        } else if(feature instanceof Geometry) {
-            if(this.checkDepth(depth)) this.drawGeometry((Geometry) feature, depth++);
         } else if(feature instanceof Polygon) {
             this.drawPolygon((Polygon) feature);
         } else if(feature instanceof LineString) {
@@ -77,54 +105,60 @@ public abstract class VectorLayer extends MapLayer {
     }
     
     private void drawMultiGeometry(MultiGeometry geometries, int depth) {
-        Iterator<Geometry> iterator = geometries.geometries();
-        while(iterator.hasNext()) {
-            this.drawGeometry(iterator.next(), depth);
-        }
+        for(VectorFeature feature: geometries) this.drawFeature(feature, depth);
     }
     
     private void drawMultiPolyon(MultiPolygon polygons) {
-        Iterator<Polygon> iterator = polygons.polygons();
-        while(iterator.hasNext()) {
-            this.drawPolygon(iterator.next());
-        }
+        for(Polygon polygon: polygons) this.drawPolygon(polygon);
     }
     
     private void drawMultiLineString(MultiLineString lines) {
-        Iterator<LineString> iterator = lines.lineStrings();
-        while(iterator.hasNext()) {
-            this.drawLineString(iterator.next());
-        }
+        for(LineString line: lines) this.drawLineString(line);
     }
     
     private void drawMultiPoint(MultiPoint points) {
-        Iterator<Point> iterator = points.points();
-        while(iterator.hasNext()) {
-            this.drawPoint(iterator.next());
-        }
-    }
-    
-    private void drawGeometry(Geometry geometry, int depth) {
-        Iterator<VectorFeature> features = geometry.features();
-        while(features.hasNext()) {
-            this.drawFeature(features.next(), depth);
-        }
+        for(Point point: points) this.drawPoint(point);
     }
     
     private void drawPolygon(Polygon polygon) {
         //TODO
+        this.polygonsRendered++;
     }
     
     private void drawLineString(LineString line) {
         //TODO
+        GeoPoint[] geo = line.getPoints();
+        double[] cart = new double[geo.length*2];
+        for(int i=0; i<geo.length; i++) {
+            Vec2d pos = this.getRenderPos(geo[i]);
+            cart[i*2] = pos.x;
+            cart[i*2 + 1] = pos.y;
+        }
+        Color color = line.getColor().withAlpha(this.getAlpha());
+        RenderUtil.drawStrokeLine(color, line.getWidth(), cart);
+        this.linesRendered++;
     }
     
     private void drawPoint(Point point) {
-        //TODO
+        Vec2d pos = this.getRenderPos(point.getPosition());
+        RenderUtil.drawRect(pos.x - 1, pos.y - 1, pos.x + 1, pos.y + 1, point.getColor().withAlpha(this.getAlpha()));
+        this.pointsRendered++;
     }
     
     private boolean checkDepth(int depth) {
         return depth <= MAX_FEATURE_DEPTH;
+    }
+
+    public int getPointsRendered() {
+        return pointsRendered;
+    }
+
+    public int getLinesRendered() {
+        return linesRendered;
+    }
+
+    public int getPolygonsRendered() {
+        return polygonsRendered;
     }
 
 }
