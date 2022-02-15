fa90133e3e5d03198984de85eee464fd510af5c7
Smyler <smyler@mail.com>
2022-02-15 14:23:37 +0100

    Implement Widget#onClickedNotInput()

--- src/main/java/fr/thesmyler/smylibgui/widgets/IWidget.java
+++ src/main/java/fr/thesmyler/smylibgui/widgets/IWidget.java
@@ -34,15 +34,15 @@ public interface IWidget {
     float getHeight();
 
     /**
-     * This method should draw the widget on the screen
-     * It is the only method in this interface which takes as parameter the absolute position relative to the Minecraft widow
+     * This method should draw the widget on the screen.
+     * It is the only method in this interface which takes as parameter the absolute position relative to the Minecraft widow.
      * 
-     * @param x the x position where to draw the widget on the screen, can be different than this.getX()
-     * @param y the y position where to draw the widget on the screen, can be different than this.getY()
-     * @param mouseX the mouse's x position on the screen
-     * @param mouseY the mouse's y position on the screen
-     * @param focused indicates whether or not this widget has its parent's focus (it will get keystrokes and so on)
-     * @param parent the parent Screen
+     * @param x         the x position where to draw the widget on the screen, can be different than this.getX()
+     * @param y         the y position where to draw the widget on the screen, can be different than this.getY()
+     * @param mouseX    the mouse's x position on the screen
+     * @param mouseY    the mouse's y position on the screen
+     * @param focused   indicates whether this widget has its parent's focus (it will get keystrokes and so on)
+     * @param parent    the parent Screen
      */
     void draw(float x, float y, float mouseX, float mouseY, boolean hovered, boolean focused, @Nullable WidgetContainer parent);
 
@@ -58,7 +58,7 @@ public interface IWidget {
     }
 
     /**
-     * If this returns false, this widget will not be notified of user actions
+     * If this returns false, this widget will not be notified of user actions.
      * 
      * @return true if this widget needs to process user inputs
      */
@@ -67,67 +67,82 @@ public interface IWidget {
     }
 
     /**
-     * Called when the user clicks this widget
-     * Can only be called if the widget is visible and takes inputs
+     * Called when the user clicks this widget.
+     * Can only be called if the widget is visible and takes inputs.
      * 
-     * @param mouseX mouse x position relative to the widget's origin
-     * @param mouseY mouse y position relative to the widget's origin
-     * @param mouseButton
-     * @param parent screen
+     * @param mouseX        mouse x position relative to the widget's origin
+     * @param mouseY        mouse y position relative to the widget's origin
+     * @param mouseButton   the button which is clicked
+     * @param parent        the parent widget container
      * 
      * @see #isVisible(WidgetContainer)
      * @see #takesInputs()
      * 
-     * @return a boolean indicating whether or not this event should propagate to widgets with lower priorities
+     * @return a boolean indicating whether this event should propagate to widgets with lower priorities
      */
     default boolean onClick(float mouseX, float mouseY, int mouseButton, @Nullable WidgetContainer parent) {
         return true;
     }
 
     /**
-     * Called when the user clicks anywhere else on the parent of this widget
-     * This will be called whether or not the widget takes inputs or is visible
+     * Called when the user clicks anywhere else on the parent of this widget.
+     * This will be called whether the widget takes inputs or is visible.
      * 
-     * @param mouseX mouse x position relative to the parent's origin
-     * @param mouseY mouse y position relative to the parent's origin
-     * @param mouseButton
-     * @param parent the containing screen that was clicked. It may not be the direct parent
+     * @param mouseX        mouse x position relative to the parent's origin
+     * @param mouseY        mouse y position relative to the parent's origin
+     * @param mouseButton   the button which was clicked
+     * @param parent        the containing screen that was clicked. It may not be the direct parent
      *
      * @see #isVisible(WidgetContainer)
      * @see #takesInputs()
-     * 
-     * @return a boolean indicating whether or not this event should propagate to widgets with lower priorities
+     *
+     * @return a boolean indicating whether this event should propagate to widgets with lower priorities
      */
     default boolean onParentClick(float mouseX, float mouseY, int mouseButton, @Nullable WidgetContainer parent) {
         return true;
     }
 
     /**
-     * Called when the user double clicks this widget
-     * Can only be called if the widget is visible and takes inputs
+     * Called when this widget is clicked but doesn't accept inputs.
+     * In contrary to other methods, returning false to this one will not focus this widget.
+     *
+     * @param mouseX        mouse x position relative to the parent's origin
+     * @param mouseY        mouse y position relative to the parent's origin
+     * @param mouseButton   the button which was clicked
+     * @param parent        the parent widget container
+     *
+     * @return whether to propagate the event
+     */
+    default boolean onClickedNotInput(float mouseX, float mouseY, int mouseButton, @Nullable WidgetContainer parent) {
+        return true;
+    }
+
+    /**
+     * Called when the user double clicks this widget.
+     * Can only be called if the widget is visible and takes inputs.
      * 
-     * @param mouseX mouse x position relative to the widget's origin
-     * @param mouseY mouse y position relative to the widget's origin
-     * @param mouseButton
-     * @param parent screen
+     * @param mouseX        mouse x position relative to the widget's origin
+     * @param mouseY        mouse y position relative to the widget's origin
+     * @param mouseButton   the button which was clicked
+     * @param parent        screen
      * 
      * @see #isVisible(WidgetContainer)
      * @see #takesInputs()
      * 
-     * @return a boolean indicating whether or not this event should propagate to widgets with lower priorities
+     * @return a boolean indicating whether this event should propagate to widgets with lower priorities
      */
     default boolean onDoubleClick(float mouseX, float mouseY, int mouseButton, @Nullable WidgetContainer parent) {
         return this.onClick(mouseX, mouseY, mouseButton, parent);
     }
 
     /**
-     * Called when the user double clicks anywhere else on the parent of this widget
-     * This will be called whether or not the widget takes inputs or is visible
+     * Called when the user double clicks anywhere else on the parent of this widget.
+     * This will be called whether the widget takes inputs or is visible.
      * 
-     * @param mouseX position relative to the parent's origin
-     * @param mouseY position relative to the parent's origin
-     * @param mouseButton
-     * @param parent the containing screen that was clicked. It may not be the direct parent
+     * @param mouseX        position relative to the parent's origin
+     * @param mouseY        position relative to the parent's origin
+     * @param mouseButton   the button which was clicked
+     * @param parent        the widget container that was clicked. It may not be the direct parent
      *
      * @see #isVisible(WidgetContainer)
      * @see #takesInputs()
@@ -139,50 +154,54 @@ public interface IWidget {
     }
 
     /**
-     * Called when this widget is being dragged by the user
-     * @param dX
-     * @param dY
-     * @param mouseButton
-     * @param parent screen
-     * @param timeSinceLastMove
+     * Called when this widget is being dragged by the user.
+     *
+     * @param mouseX            the current X position of the mouse
+     * @param mouseY            the current Y position of the mouse
+     * @param dX                the X offset of the new position relative to the old one
+     * @param dY                the Y offset of the new position relative to the old one
+     * @param mouseButton       the button which is being pressed
+     * @param parent            the widget container
+     * @param timeSinceLastMove the time since this method was last called for the same press, in milliseconds
      */
     default void onMouseDragged(float mouseX, float mouseY, float dX, float dY, int mouseButton, @Nullable WidgetContainer parent, long timeSinceLastMove) {}
 
     /**
-     * Called when the mouse is released over this widget
+     * Called when the mouse is released over this widget.
      * 
-     * @param mouseX position relative to the parent's origin
-     * @param mouseY position relative to the parent's origin
-     * @param button
+     * @param mouseX        position relative to the parent's origin
+     * @param mouseY        position relative to the parent's origin
+     * @param button        the button which was released
      * @param draggedWidget the widget the user was dragging and drop on this widget
      *
      */
     default void onMouseReleased(float mouseX, float mouseY, int button, @Nullable IWidget draggedWidget) {}
 
     /**
-     * Called between the time the events are processed and the screen is drawn
-     * @param mouseX
-     * @param mouseY
-     * @param parent screen
+     * Called between the time the events are processed and the screen is drawn.
+     *
+     * @param mouseX    the X position of the mouse
+     * @param mouseY    the Y position of the mouse
+     * @param parent    the parent widget container
      */
     default void onUpdate(float mouseX, float mouseY, @Nullable WidgetContainer parent) {}
 
     /**
-     * Called when a key is typed
+     * Called when a key is typed.
      * 
-     * @param typedChar
-     * @param key
-     * @param parent screen
+     * @param typedChar     the character represented by the key that was pressed on the current keyboard layout
+     * @param key           the physical key that was pressed
+     * @param parent        the parent widget container
      */
     default void onKeyTyped(char typedChar, Key key, @Nullable WidgetContainer parent) {}
 
     /**
      * Called when the mouse is over this widget and the wheel is turned
      * 
-     * @param mouseX position relative to the widget
-     * @param mouseY position relative to the widget
-     * @param parent screen
-     * @param amount
+     * @param mouseX    position relative to the widget
+     * @param mouseY    position relative to the widget
+     * @param parent    the parent widget container
+     * @param amount    how much was the wheel turned
      */
     default boolean onMouseWheeled(float mouseX, float mouseY, int amount, @Nullable WidgetContainer parent) {
         return true;
@@ -191,8 +210,7 @@ public interface IWidget {
     /**
      * @return the text to draw as a tooltip when this widget is hovered, or null if no tooltip should be displayed
      */
-    @Nullable
-    default String getTooltipText() {
+    @Nullable default String getTooltipText() {
         return null;
     }
 
@@ -208,6 +226,5 @@ public interface IWidget {
      * Called when the screen is closed or this widget is removed from the screen
      */
     default void onRemoved() {}
-
 
 }
---
+++ src/test/java/fr/thesmyler/smylibgui/widgets/buttons/TextButtonWidgetTest.java
@@ -0,0 +1,15 @@
+package fr.thesmyler.smylibgui.widgets.buttons;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class TextButtonWidgetTest {
+
+    @Test
+    public void testActivation() {
+        AtomicInteger clickCounter = new AtomicInteger();
+
+    }
+
+}
