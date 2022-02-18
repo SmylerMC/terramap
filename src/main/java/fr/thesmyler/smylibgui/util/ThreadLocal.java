473715ea905b871f826d01b595bad5b87d69a53f
Smyler <smyler@mail.com>
2022-02-18 10:53:49 +0100

    Basic dummy devices implementation

---
+++ src/main/java/fr/thesmyler/smylibgui/util/ThreadLocal.java
@@ -0,0 +1,25 @@
+package fr.thesmyler.smylibgui.util;
+
+import java.util.function.Supplier;
+
+/**
+ * A ThreadLocal that uses a supplier to get its initial value.
+ *
+ * @param <T>   the type of the enclosed object
+ *
+ * @author SmylerMC
+ */
+public class ThreadLocal<T> extends java.lang.ThreadLocal<T> {
+
+    private final Supplier<T> supplier;
+
+    public ThreadLocal(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    @Override
+    protected T initialValue() {
+        return this.supplier.get();
+    }
+
+}
