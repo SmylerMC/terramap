019746013adf81425f4be70e6cfeebaf5d0d2a34
Smyler <smyler@mail.com>
2022-02-18 01:09:36 +0100

    Implement lwjgl2 devices wrappers

---
+++ src/main/java/fr/thesmyler/smylibgui/devices/lwjgl2/ScaledResolutionGameWindow.java
@@ -0,0 +1,53 @@
+package fr.thesmyler.smylibgui.devices.lwjgl2;
+
+import fr.thesmyler.smylibgui.devices.GameWindow;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
+
+public class ScaledResolutionGameWindow implements GameWindow {
+
+    private float width = 1f;
+    private float height = 1f;
+    private int nativeWidth = 1;
+    private int nativeHeight = 1;
+    private int scale = 1;
+
+    @Override
+    public float getWindowWidth() {
+        return this.width;
+    }
+
+    @Override
+    public float getWindowHeight() {
+        return this.height;
+    }
+
+    @Override
+    public int getNativeWindowWidth() {
+        return this.nativeWidth;
+    }
+
+    @Override
+    public int getNativeWindowHeight() {
+        return this.nativeHeight;
+    }
+
+    @Override
+    public int getScaleFactor() {
+        return this.scale;
+    }
+
+    @SubscribeEvent
+    public void onRender(TickEvent.RenderTickEvent event) {
+        ScaledResolution res = new ScaledResolution(Minecraft.getMinecraft());
+        Minecraft mc = Minecraft.getMinecraft();
+        this.scale = res.getScaleFactor();
+        this.width = (float) res.getScaledWidth_double();
+        this.height = (float) res.getScaledHeight_double();
+        this.nativeWidth = mc.displayWidth;
+        this.nativeHeight = mc.displayHeight;
+    }
+
+}
