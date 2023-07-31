bf07127bf4ed7642439af2953b8060484ac336e1
Smyler <smyler@mail.com>
2023-07-30 17:59:36 -0700

    Added MapWidgetTest#canRestoreMapState

--- src/test/java/fr/thesmyler/terramap/gui/widgets/map/MapWidgetTest.java
+++ src/test/java/fr/thesmyler/terramap/gui/widgets/map/MapWidgetTest.java
@@ -1,29 +1,22 @@
 package fr.thesmyler.terramap.gui.widgets.map;
 
-import fr.thesmyler.smylibgui.SmyLibGuiTest;
+import com.google.gson.JsonPrimitive;
 import fr.thesmyler.smylibgui.container.TestingWidgetContainer;
 import fr.thesmyler.terramap.MapContext;
-import fr.thesmyler.terramap.TerramapMod;
+import fr.thesmyler.terramap.TerramapTest;
 import fr.thesmyler.terramap.gui.widgets.map.layer.RasterMapLayer;
 import fr.thesmyler.terramap.maps.SavedLayerState;
 import fr.thesmyler.terramap.maps.SavedMapState;
 import fr.thesmyler.terramap.maps.raster.MapStylesLibrary;
+import fr.thesmyler.terramap.util.geo.GeoPointImmutable;
 import fr.thesmyler.terramap.util.math.Vec2dImmutable;
-import org.apache.logging.log4j.LogManager;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static fr.thesmyler.terramap.gui.widgets.map.MapLayerLibrary.RASTER_LAYER_ID;
 import static fr.thesmyler.terramap.util.geo.GeoPointTest.PARIS;
 import static org.junit.jupiter.api.Assertions.*;
 
-class MapWidgetTest extends SmyLibGuiTest {
-
-    @BeforeEach
-    public void reloadMapStyles() {
-        TerramapMod.logger = LogManager.getLogger("Map unit test");
-        MapStylesLibrary.reload();
-    }
+class MapWidgetTest extends TerramapTest {
 
     @Test
     void canSaveMapWidgetToSavedMapState() throws InterruptedException {
@@ -67,6 +60,50 @@ class MapWidgetTest extends SmyLibGuiTest {
         assertEquals(0.5f, savedStamen.alpha);
         assertEquals(0, savedStamen.cartesianOffset.subtract(0.042d, -0.24d).norm(), 1e-5d);
         assertEquals(15f, savedStamen.rotationOffset, 1e-5f);
+
+    }
+
+    @Test
+    public void canRestoreMapState() throws InterruptedException {
+
+        TestingWidgetContainer screen = new TestingWidgetContainer(60, 500f, 500f);
+        MapWidget map = new MapWidget(0f, 0f, 0,  500f, 500f, MapContext.FULLSCREEN, 1f);
+        screen.moveMouse(750, 750, 1000);
+        screen.addWidget(map);
+
+        // Let's start in some random state
+        RasterMapLayer rasterLayer = (RasterMapLayer) map.createLayer(RASTER_LAYER_ID);
+        rasterLayer.setTiledMap(MapStylesLibrary.getBaseMaps().get("osm"));
+        map.setLayerZ(rasterLayer, -1);
+        map.getController().setZoomStaticLocation(PARIS);
+        map.getController().setZoom(18, true);
+        screen.runFor(1000);
+        map.getController().moveLocationToCenter(PARIS, true);
+        screen.runFor(1000);
+        map.getController().setRotationStaticLocation(PARIS);
+        map.getController().setRotation(45f, true);
+
+        // A completely different saved state
+        SavedMapState state = new SavedMapState();
+        state.center.set(GeoPointImmutable.ORIGIN);
+        state.zoom = 0;
+        state.rotation = 0;
+        SavedLayerState layerState = new SavedLayerState();
+        layerState.type = RASTER_LAYER_ID;
+        layerState.settings.add("style", new JsonPrimitive("stamen_terrain"));
+        state.layers.add(layerState);
+
+        // And we try restoring...
+        map.restore(state);
+        screen.doTick();
+
+        assertEquals(GeoPointImmutable.ORIGIN, map.getController().getCenterLocation().getImmutable());
+        assertEquals(0, map.getController().getZoom());
+        assertEquals(0, map.getController().getRotation());
+        assertEquals(2, map.getLayers().size()); // Input layer and raster layer
+        MapLayer layer = map.getLayers().get(1);
+        assertTrue(layer instanceof RasterMapLayer);
+        assertEquals("stamen_terrain", ((RasterMapLayer) layer).getTiledMap().getId());
 
     }
 
