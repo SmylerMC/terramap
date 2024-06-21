896bd6c1f1c4b132d2228b04c7afcb617ec26ca4
Smyler <smyler@mail.com>
2024-06-21 19:44:47 +0200

    Remove everything warp related

--- forge/src/main/java/fr/thesmyler/terramap/network/TerramapNetworkManager.java
+++ forge/src/main/java/fr/thesmyler/terramap/network/TerramapNetworkManager.java
@@ -12,29 +12,6 @@ import fr.thesmyler.terramap.network.playersync.SP2CPlayerSyncPacket;
 import fr.thesmyler.terramap.network.playersync.SP2CPlayerSyncPacket.S2CPlayerSyncPacketHandler;
 import fr.thesmyler.terramap.network.playersync.SP2CRegistrationExpiresPacket;
 import fr.thesmyler.terramap.network.playersync.SP2CRegistrationExpiresPacket.S2CRegistrationExpiresPacketHandler;
-import fr.thesmyler.terramap.network.warps.C2SPCreateWarpPacket;
-import fr.thesmyler.terramap.network.warps.C2SPCreateWarpPacket.C2SPCreateWarpPacketHandler;
-import fr.thesmyler.terramap.network.warps.C2SPEditWarpPacket;
-import fr.thesmyler.terramap.network.warps.C2SPEditWarpPacket.C2SPEditWarpPacketHandler;
-import fr.thesmyler.terramap.network.warps.C2SPRequestMultiWarpPacket;
-import fr.thesmyler.terramap.network.warps.C2SPRequestMultiWarpPacket.C2SPRequestMultiWarpPacketHandler;
-import fr.thesmyler.terramap.network.warps.C2SPRequestWarpPacket;
-import fr.thesmyler.terramap.network.warps.C2SPRequestWarpPacket.C2SPRequestWarpPacketHandler;
-import fr.thesmyler.terramap.network.warps.SP2CCreateWarpConfirmationPacket;
-import fr.thesmyler.terramap.network.warps.SP2CCreateWarpConfirmationPacket.SP2CCreateWarpConfirmationPacketProxyHandler;
-import fr.thesmyler.terramap.network.warps.SP2CCreateWarpConfirmationPacket.SP2CCreateWarpConfirmationPacketServerHandler;
-import fr.thesmyler.terramap.network.warps.SP2CEditWarpConfirmationPacket;
-import fr.thesmyler.terramap.network.warps.SP2CEditWarpConfirmationPacket.SP2CEditWarpConfirmationPacketProxyHandler;
-import fr.thesmyler.terramap.network.warps.SP2CEditWarpConfirmationPacket.SP2CEditWarpConfirmationPacketServerHandler;
-import fr.thesmyler.terramap.network.warps.SP2CMultiWarpPacket;
-import fr.thesmyler.terramap.network.warps.SP2CMultiWarpPacket.SP2CMultiWarpPacketProxyHandler;
-import fr.thesmyler.terramap.network.warps.SP2CMultiWarpPacket.SP2CMultiWarpPacketServerHandler;
-import fr.thesmyler.terramap.network.warps.SP2CWarpCommandPacket;
-import fr.thesmyler.terramap.network.warps.SP2CWarpCommandPacket.SP2CWarpCommandPacketProxyHandler;
-import fr.thesmyler.terramap.network.warps.SP2CWarpCommandPacket.SP2CWarpCommandPacketServerHandler;
-import fr.thesmyler.terramap.network.warps.SP2CWarpPacket;
-import fr.thesmyler.terramap.network.warps.SP2CWarpPacket.SP2CWarpPacketProxyHandler;
-import fr.thesmyler.terramap.network.warps.SP2CWarpPacket.SP2CWarpPacketServerHandler;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
@@ -58,15 +35,6 @@ public abstract class TerramapNetworkManager {
         registerTerramapS2C(S2C_TERRAMAP_HELLO_DISCRIMINATOR, S2CTerramapHelloPacketHandler.class, S2CTerramapHelloPacket.class);
         registerTerramapS2C(S2C_TERRAMAP_TPCMD_DISCRIMINATOR, S2CTpCommandPacketHandler.class, S2CTpCommandPacket.class);
         registerTerramapS2C(S2C_TERRAMAP_MAPSTYLE_DISCRIMINATOR, SP2CMapStylePacketTerramapHandler.class, SP2CMapStylePacket.class);
-        registerTerramapC2S(C2S_TERRAMAP_REQUEST_WARP_DISCRIMINATOR, C2SPRequestWarpPacketHandler.class, C2SPRequestWarpPacket.class);
-        registerTerramapS2C(S2C_TERRAMAP_WARP_DISCRIMINATOR, SP2CWarpPacketServerHandler.class, SP2CWarpPacket.class);
-        registerTerramapC2S(C2S_TERRAMAP_REQUEST_MULTI_WARP_DISCRIMINATOR, C2SPRequestMultiWarpPacketHandler.class, C2SPRequestMultiWarpPacket.class);
-        registerTerramapS2C(S2C_TERRAMAP_MULTI_WARP_DISCRIMINATOR, SP2CMultiWarpPacketServerHandler.class, SP2CMultiWarpPacket.class);
-        registerTerramapC2S(C2S_TERRAMAP_CREATE_WARP_DISCRIMINATOR, C2SPCreateWarpPacketHandler.class, C2SPCreateWarpPacket.class);
-        registerTerramapS2C(S2C_TERRAMAP_CREATE_WARP_CONFIRMATION_DISCRIMINATOR, SP2CCreateWarpConfirmationPacketServerHandler.class, SP2CCreateWarpConfirmationPacket.class);
-        registerTerramapC2S(C2S_TERRAMAP_EDIT_WARP_DISCRIMINATOR, C2SPEditWarpPacketHandler.class, C2SPEditWarpPacket.class);
-        registerTerramapS2C(S2C_TERRAMAP_EDIT_WARP_CONFIRMATION_DISCRIMINATOR, SP2CEditWarpConfirmationPacketServerHandler.class, SP2CEditWarpConfirmationPacket.class);
-        registerTerramapS2C(S2C_TERRAMAP_WARP_COMMAND_DISCRIMINATOR, SP2CWarpCommandPacketServerHandler.class, SP2CWarpCommandPacket.class);
 
         registerMapsyncCP2S(C2SP_MAPSYNC_REGISTER_DISCRIMINATOR, C2SRegisterForUpdatesPacketHandler.class, C2SPRegisterForUpdatesPacket.class);
         registerMapsyncSP2C(SP2C_MAPSYNC_PLAYERSYNC_DISCRIMINATOR, S2CPlayerSyncPacketHandler.class, SP2CPlayerSyncPacket.class);
@@ -74,15 +42,6 @@ public abstract class TerramapNetworkManager {
 
         registerSledgehammerP2C(P2C_SH_HELLO_DISCRIMINATOR, P2CSledgehammerHelloPacketHandler.class, P2CSledgehammerHelloPacket.class);
         registerSledgehammerP2C(P2C_SH_MAPSTYLE_DISCRIMINATOR, SP2CMapStylePacketSledgehammerHandler.class, SP2CMapStylePacket.class);
-        registerSledgehammerC2P(C2P_SH_REQUEST_WARP_DISCRIMINATOR, C2SPRequestWarpPacketHandler.class, C2SPRequestWarpPacket.class);
-        registerSledgehammerP2C(P2C_SH_WARP_DISCRIMINATOR, SP2CWarpPacketProxyHandler.class, SP2CWarpPacket.class);
-        registerSledgehammerC2P(C2P_SH_REQUEST_MULTI_WARP_DISCRIMINATOR, C2SPRequestMultiWarpPacketHandler.class, C2SPRequestMultiWarpPacket.class);
-        registerSledgehammerP2C(P2C_SH_MULTI_WARP_DISCRIMINATOR, SP2CMultiWarpPacketProxyHandler.class, SP2CMultiWarpPacket.class);
-        registerSledgehammerC2P(C2P_SH_CREATE_WARP_DISCRIMINATOR, C2SPCreateWarpPacketHandler.class, C2SPCreateWarpPacket.class);
-        registerSledgehammerP2C(P2C_SH_CREATE_WARP_CONFIRMATION_DISCRIMINATOR, SP2CCreateWarpConfirmationPacketProxyHandler.class, SP2CCreateWarpConfirmationPacket.class);
-        registerSledgehammerC2P(C2P_SH_EDIT_WARP_DISCRIMINATOR, C2SPEditWarpPacketHandler.class, C2SPEditWarpPacket.class);
-        registerSledgehammerP2C(P2C_SH_EDIT_WARP_CONFIRMATION_DISCRIMINATOR, SP2CEditWarpConfirmationPacketProxyHandler.class, SP2CEditWarpConfirmationPacket.class);
-        registerSledgehammerP2C(P2C_SH_WARP_COMMAND_DISCRIMINATOR, SP2CWarpCommandPacketProxyHandler.class, SP2CWarpCommandPacket.class);
 
     }
 
