1aa0188437d1ae7ca5a202ca8893397b2c38b3bf
Smyler <smyler@mail.com>
2024-07-21 13:25:16 +0200

    Fix text being rendered with z offset

---
+++ smylib/fabric/src/main/java/net/smyler/smylib/gui/PatchedFont.java
@@ -0,0 +1,8 @@
+package net.smyler.smylib.gui;
+
+/**
+ * Exposes methods declared in {@link net.smyler.smylib.mixins.FontMixinNoShadowOffset}.
+ */
+public interface PatchedFont {
+    void smylib$setCancelShadowOffset(boolean cancelShadowOffset);
+}
