1b20069f536e32bd0d83bffe311c201c7ffc5fb2
Smyler <smyler@mail.com>
2024-08-29 01:46:03 +0200

    Enforce max concurrent requests limitations in http client

--- fabric/src/test/java/net/smyler/terramap/http/TerramapHttpClientTest.java
+++ fabric/src/test/java/net/smyler/terramap/http/TerramapHttpClientTest.java
@@ -3,7 +3,10 @@ package net.smyler.terramap.http;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 
 import static org.apache.logging.log4j.LogManager.getLogger;
 
@@ -11,9 +14,9 @@ import static org.apache.logging.log4j.LogManager.getLogger;
 public class TerramapHttpClientTest {
 
     @Test
-    void canRequest() throws ExecutionException, InterruptedException {
+    void canRequest() throws ExecutionException, InterruptedException, IOException {
         Logger logger = getLogger("HTTP test");
-        HttpClient client = new TerramapHttpClient(logger, new MemoryCache());
+        HttpClient client = new TerramapHttpClient(logger, Files.createTempDirectory("terramap-tests"));
         String content = new String(client.get("https://tile-c.openstreetmap.fr/hot/7/66/38.png").get());
         content = new String(client.get("https://tile-c.openstreetmap.fr/hot/7/66/38.png").get());
     }
