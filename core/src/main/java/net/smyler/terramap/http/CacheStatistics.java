027d45a853ac09ec06b43acdc44629328b7aa057
Smyler <smyler@mail.com>
2024-10-15 16:46:20 +0200

    Do not use records in 1.12.2, that's Java 8...

--- core/src/main/java/net/smyler/terramap/http/CacheStatistics.java
+++ core/src/main/java/net/smyler/terramap/http/CacheStatistics.java
@@ -2,12 +2,23 @@ package net.smyler.terramap.http;
 
 /**
  * Cache usage statistics.
- *
- * @param entries   number of entries in the cache
- * @param size      total storage space used by the cache
- * @param type      the type of cache in use
  */
-public record CacheStatistics(long entries, long size, CacheType type) {
+public class CacheStatistics {
+
+    public final long entries;
+    public final long size;
+    public final CacheType type;
+
+    /**
+     * @param entries   number of entries in the cache
+     * @param size      total storage space used by the cache
+     * @param type      the type of cache in use
+     */
+    public CacheStatistics(long entries, long size, CacheType type) {
+        this.entries = entries;
+        this.size = size;
+        this.type = type;
+    }
 
     public enum CacheType {
 
